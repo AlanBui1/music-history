caecfee73006bf55fa0f3cdbf28bcffec37f3ea7
AlanBui1 <alanb3350@gmail.com>
2022-12-24 11:51:35 -0500

    tested practice test 1

--- _posts/autograde.py
+++ _posts/autograde.py
@@ -1,15 +1,16 @@
 from datetime import date
 today = str(date.today())
+username = "Alan"
 
 AnsFile = "_posts/2022-12-24-practice1.md"
 questions = []
 answers = []
-FILENAME = today+"-sub1.md"
+FILENAME = "_posts/"+today+"-sub1.md"
 
 with open(AnsFile.replace(".md", "answers.md"), "r") as inFile:
     ind = 0
     while True:
-        line = inFile.readline()
+        line = inFile.readline().strip()
         if line == "":
             break
     
@@ -26,7 +27,39 @@ for i in range(len(allAns)):
 
 outFile = open(FILENAME, "w")
 
+outFile.write('''---
+title: Practice Exam 1 Sub 1
+categories: [Practice Exams]
+permalink: /practice1/sub1/
+---
 
+### Date: '''+today+'''
 
+### Submitted by: '''+username+'''
+
+## Submission:
+
+| Question | Your Answer | Score | Intended Answer |
+| :---        |    :----:   |  :----: |        ---: |
+'''
+) 
+
+wrong = []
+correct = 0
+for i in range(len(questions)):
+    score = "0/1"
+    if allAns[i].strip() == answers[i].strip():
+        score = "1/1"
+        correct += 1
+    else:
+        wrong.append(questions[i])
+    outFile.write("| "+questions[i]+" | "+allAns[i] + " | " + score + " | " + answers[i] + " |\n")
+
+outFile.write('''
+
+## Final Score: '''+str(correct)+"/"+str(len(questions)) +'''
+
+'''
+)
 
 outFile.close()
