14454fa1ad506c042ff3c9ceee10430dcc19b1aa
AlanBui1 <alanb3350@gmail.com>
2022-12-30 00:04:08 -0500

    changed practice exam gen

---
+++ questionGen/makeProblems.py
@@ -0,0 +1,53 @@
+from random import randint, shuffle
+
+class Problem:
+    def __init__(self, q, a, era, categories, theme):
+        self.question = q
+        self.answer = a
+        self.era = era
+        self.categories = categories
+        self.choices = set()
+        self.choices.add(a)
+        self.options = []
+        self.theme = theme
+
+problemsByTheme = {}
+allProblems = []
+
+with open("questionGen/questions.txt", "r") as questionFile, open("questionGen/answers.txt", "r") as answerFile, open("questionGen/era.txt", "r") as eraFile, open("questionGen/categories.txt", "r") as categoryFile, open("questionGen/themes.txt", "r") as themeFile:
+    while 1:
+        q = questionFile.readline()
+        a = answerFile.readline()
+        e = eraFile.readline()
+        cats = categoryFile.readline().split()
+        theme = themeFile.readline().strip()
+
+        if q == "":
+            break
+
+        if theme not in problemsByTheme:
+            problemsByTheme[theme] = []
+
+        problemsByTheme[theme].append(Problem(q, a, e, cats, theme))
+        allProblems.append(Problem(q, a, e, cats, theme))
+
+shuffle(allProblems)
+
+#setting random answers
+with open("questionGen/allProblems.txt", "w") as outFile:
+    for i in range(len(allProblems) - 1):
+        while len(allProblems[i].choices) < 4:
+            randNum = randint(0, len(problemsByTheme[allProblems[i].theme])-1)
+            allProblems[i].choices.add(problemsByTheme[allProblems[i].theme][randNum].answer)
+
+        for op in allProblems[i].choices:
+            allProblems[i].options.append(op)
+
+        shuffle(allProblems[i].options)
+
+        outFile.write(allProblems[i].theme +"\n")
+        outFile.write(allProblems[i].era)
+        outFile.write(allProblems[i].question)
+        for op in allProblems[i].options:
+            outFile.write(op.strip()+"[]\n")
+        outFile.write(allProblems[i].answer)
---
+++ questionGen/mixedTest.py
@@ -0,0 +1,36 @@
+from random import shuffle
+from datetime import date
+today = str(date.today())
+
+FILENAMETOOUTPUT = '_posts/practice3.md'
+PAGETITLE = "Practice Exam 3 Questions"
+PERMALINK = "/practice3qs/"
+
+with open("questionGen/allProblems.txt") as inFile, open(FILENAMETOOUTPUT, "w") as outFile:
+    outFile.write('''---
+title: '''+PAGETITLE+'''
+categories: [Practice Exams]
+permalink: '''+PERMALINK+'''
+---
+
+''')
+
+
+    while True:
+        theme = inFile.readline()
+        if theme == "":
+            break
+
+        era = inFile.readline()
+        question = inFile.readline()
+        possible = []
+        for i in range(4):
+            possible.append(inFile.readline())
+        shuffle(possible)
+
+        answer = inFile.readline()
+
+        outFile.write(question + "\n")
+        for ans in possible:
+            outFile.write("- "+ans)
+        outFile.write("\n")
